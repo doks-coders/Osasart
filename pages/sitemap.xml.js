27a6d586d0399b5434c902cc7c6c953773f91344
guonnie <guonnie@gmail.com>
2022-07-07 01:32:36 +0100

    edited xml

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -46,6 +46,8 @@ const dynamicPaths = allPosts.map( post => {
 
 const allPaths =[ ...staticPaths , ...dynamicPaths ];
 
+
+console.log(allPaths)
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${allPaths
